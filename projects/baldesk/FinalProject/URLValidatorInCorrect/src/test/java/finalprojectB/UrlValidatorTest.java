1d0c16cb6673a46501ab4efcf56419c52490fdc3
Baldesk <baldesk@oregonstate.edu>
2018-03-19 14:51:25 -0700

    Finished Manual Testing began partitioning and writing testIsValid(). Also corrected bug in ResultPair.java where the value was being negated on line 30. Updates version control for URLValidatorCorrect as well.

--- projects/baldesk/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ projects/baldesk/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -13,9 +13,31 @@ import junit.framework.TestCase;
 
 public class UrlValidatorTest extends TestCase
 {
+   // Input Partitions:
+   ResultPair[] test_scheme = {
+           new ResultPair("http://", true),
+           new ResultPair("https://", true),
+           new ResultPair("ftp://", true),
+           new ResultPair("3ht://", false),
+           new ResultPair("http:/", false),
+           new ResultPair("http:", false),
+           new ResultPair("http/", false),
+           new ResultPair("://", false),
+           new ResultPair("", true)
+   };
+
+
+
+   // isValid Options Partitions
+   public long ALLOW_ALL_SCHEMES = 1;
+   public long ALLOOW_2_SLASHES = 2;
+   public long NO_FRAGMENTS = 4;
+   public long ALLOW_LOCAL_URLS = 8;
+
 
 
    public UrlValidatorTest(String testName) {
+
       super(testName);
    }
 
@@ -27,7 +49,7 @@ public class UrlValidatorTest extends TestCase
 
 
 
-      UrlValidator urlVal = new UrlValidator();
+      UrlValidator urlVal = new UrlValidator(); // Default
       //assertTrue(urlVal.isValid("http://www.google.com")); // FAILURE: AssertionFailedError
       //assertTrue(urlVal.isValid("http://google.com")); // FAILURE: AssertionFailedError
       //assertTrue(urlVal.isValid("https://www.google.com")); FAILURE: AssertionFailedError
@@ -48,25 +70,25 @@ public class UrlValidatorTest extends TestCase
 
 
       ///////////////////////////////////////////////////////////////////////
-      urlVal = new UrlValidator(null, null, 1);
+      urlVal = new UrlValidator(null, null, 1); // Allow all schemes
 
       assertTrue(urlVal.isValid("http://www.google.com")); // PASSED
       assertTrue(urlVal.isValid("http://google.com")); // PASSED
       //assertTrue(urlVal.isValid("https://www.google.com")); // FAILURE: Exception thrown: Regular expressions missing
 
-      assertTrue(urlVal.isValid("https://google.com")); // FAILURE: Exception thrown: Regular expressions missing
-      assertFalse(urlVal.isValid("http://google"));
-      assertTrue(urlVal.isValid("https://www.google.com"));
-      assertFalse(urlVal.isValid("https://google.c"));
-      assertFalse(urlVal.isValid("https//google.com"));
-      assertFalse(urlVal.isValid("https:/google.com"));
-      assertFalse(urlVal.isValid("google.com"));
-      assertFalse(urlVal.isValid("www.google.com"));
-      assertFalse(urlVal.isValid("300.0.0.0"));
-      assertFalse(urlVal.isValid(".0.0.0"));
-      assertFalse(urlVal.isValid(""));
-      assertFalse(urlVal.isValid(" "));
-      assertFalse(urlVal.isValid(null));
+      //assertTrue(urlVal.isValid("https://google.com")); // FAILURE: Exception thrown: Regular expressions missing
+      //assertFalse(urlVal.isValid("http://google")); // FAILURE: Assertion Failed
+      //assertTrue(urlVal.isValid("https://www.google.com")); // FAILURE: Exception thrown: Regular expressions missing
+      //assertFalse(urlVal.isValid("https://google.c")); // FAILURE: Exception thrown: Regular expressions missing
+      assertFalse(urlVal.isValid("https//google.com")); // PASSED
+      assertFalse(urlVal.isValid("https:/google.com")); // PASSED
+      assertFalse(urlVal.isValid("google.com")); // PASSED
+      assertFalse(urlVal.isValid("www.google.com")); // PASSED
+      assertFalse(urlVal.isValid("300.0.0.0")); // PASSED
+      assertFalse(urlVal.isValid(".0.0.0")); // PASSED
+      assertFalse(urlVal.isValid("")); // PASSED
+      assertFalse(urlVal.isValid(" ")); // PASSED
+      assertFalse(urlVal.isValid(null)); // PASSED
 
    }
 
@@ -75,30 +97,68 @@ public class UrlValidatorTest extends TestCase
    {
       //You can use this function to implement your First Partition testing
 
+      UrlValidator urlVal = new UrlValidator(); // Default
+      //assertTrue(urlVal.isValid("http://www.google.com")); // FAILURE: AssertionFailedError
+      //assertTrue(urlVal.isValid("http://google.com")); // FAILURE: AssertionFailedError
+      //assertTrue(urlVal.isValid("https://www.google.com")); FAILURE: AssertionFailedError
+      //assertTrue(urlVal.isValid("https://google.com")); // FAILURE: AssertionFailedError
+      assertFalse(urlVal.isValid("http://google")); // PASSED
+      //assertTrue(urlVal.isValid("https://www.google.com")); // FAILURE: AssertionFailedError
+      assertFalse(urlVal.isValid("https://google.c")); // PASSED
+      assertFalse(urlVal.isValid("https//google.com")); //PASSED
+      assertFalse(urlVal.isValid("https:/google.com")); // PASSED
+      assertFalse(urlVal.isValid("google.com")); // PASSED
+      assertFalse(urlVal.isValid("www.google.com")); // PASSED
+      assertFalse(urlVal.isValid("300.0.0.0")); // PASSED
+      assertFalse(urlVal.isValid(".0.0.0")); // PASSED
+      assertFalse(urlVal.isValid("")); // PASSED
+      assertFalse(urlVal.isValid(" ")); // PASSED
+      assertFalse(urlVal.isValid(null)); // PASSED
 
+
+
+      ///////////////////////////////////////////////////////////////////////
+      urlVal = new UrlValidator(null, null, 1); // Allow all schemes
+
+      assertTrue(urlVal.isValid("http://www.google.com")); // PASSED
+      assertTrue(urlVal.isValid("http://google.com")); // PASSED
+      //assertTrue(urlVal.isValid("https://www.google.com")); // FAILURE: Exception thrown: Regular expressions missing
+
+      //assertTrue(urlVal.isValid("https://google.com")); // FAILURE: Exception thrown: Regular expressions missing
+      //assertFalse(urlVal.isValid("http://google")); // FAILURE: Assertion Failed
+      //assertTrue(urlVal.isValid("https://www.google.com")); // FAILURE: Exception thrown: Regular expressions missing
+      //assertFalse(urlVal.isValid("https://google.c")); // FAILURE: Exception thrown: Regular expressions missing
+      assertFalse(urlVal.isValid("https//google.com")); // PASSED
+      assertFalse(urlVal.isValid("https:/google.com")); // PASSED
+      assertFalse(urlVal.isValid("google.com")); // PASSED
+      assertFalse(urlVal.isValid("www.google.com")); // PASSED
+      assertFalse(urlVal.isValid("300.0.0.0")); // PASSED
+      assertFalse(urlVal.isValid(".0.0.0")); // PASSED
+      assertFalse(urlVal.isValid("")); // PASSED
+      assertFalse(urlVal.isValid(" ")); // PASSED
+      assertFalse(urlVal.isValid(null)); // PASSED
 
 
 
    }
 
-   public void testYourSecondPartition(){
-      //You can use this function to implement your Second Partition testing
-
-
-
-
-
+   // String builder?
+   public void testURLBuilder(String scheme, String path)
+   {
 
    }
-   //You need to create more test cases for your Partitions if you need to
+
+
+
 
    public void testIsValid()
    {
       //You can use this function for programming based testing
+      System.out.print("Begin Random Testing");
+      // loop through each option
 
-
-
-
+            // loop through each input partition
+                  //if allowed option == current option then should be true
 
 
 
